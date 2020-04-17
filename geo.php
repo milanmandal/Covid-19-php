0a8c72f1688c47fcc81e51eb8db3d473523c6d52
milanmandal <51320381+milanmandal@users.noreply.github.com>
2020-04-17 18:31:54 +0530

    Update geo.php

--- geo.php
+++ geo.php
@@ -38,49 +38,22 @@ $maps_array= json_decode($maps_json,true);
             .mySlides {display: none;}
             img {vertical-align: middle;}
 
-            /* Slideshow container */
+           
             .slideshow-container {
             max-width: 1000px;
             position: relative;
             margin-left:10px;
             }
 
-            /* Caption text */
-            .text {
-            color: #f2f2f2;
-            font-size: 15px;
-            padding: 8px 12px;
-            position: absolute;
-            bottom: 8px;
-            width: 100%;
-            text-align: center;
-            }
+          
 
-            /* Number text (1/3 etc) */
-            .numbertext {
-            color: #f2f2f2;
-            font-size: 12px;
-            padding: 8px 12px;
-            position: absolute;
-            top: 0;
-            }
-
-            /* The dots/bullets/indicators 
-            .dot {
-            height: 15px;
-            width: 15px;
-            margin: 0 2px;
-            background-color: #bbb;
-            border-radius: 50%;
-            display: inline-block;
-            transition: background-color 0.6s ease;
-            }*/
+           
 
             .active {
             background-color: #717171;
             }
 
-            /* Fading animation */
+          
             .fade {
             -webkit-animation-name: fade;
             -webkit-animation-duration: 1.5s;
@@ -98,9 +71,9 @@ $maps_array= json_decode($maps_json,true);
             to {opacity: 1}
             }
 
-            /* On smaller screens, decrease text size */
+            
             @media only screen and (max-width: 300px) {
-            .text {font-size: 11px}
+            
             }
 
             
@@ -293,26 +266,22 @@ $maps_array= json_decode($maps_json,true);
                 <div class="slideshow-container">
 
                 <div class="mySlides fade">
-                <div class="text"></div>
-                <div class="numbertext"></div>
+               
                 <img src="safe1.jpg" style="width:80%;height:40%;margin-top:80px;">
                 </div>
 
                 <div class="mySlides fade">
-                <div class="text"></div>
-                <div class="numbertext"></div>
+               
                 <img src="safe2.jpg" style="width:80%;height:40%;margin-top:80px;">
                 </div>
 
                 <div class="mySlides fade">
-                <div class="text"></div>
-                <div class="numbertext"></div>
+                
                 <img src="safe3.bmp" style="width:80%;height:40%;margin-top:80px;">
                 </div>
 
                 <div class="mySlides fade">
-                <div class="text"></div>
-                <div class="numbertext"></div>
+               
                 <img src="safe4.jpg" style="width:80%;height:40%;margin-top:80px;">
                 </div>
 
@@ -321,12 +290,7 @@ $maps_array= json_decode($maps_json,true);
                 <br>
                 
 
-                <div style="text-align:center">
-                <span class="dot"></span> 
-                <span class="dot"></span> 
-                <span class="dot"></span> 
-                <span class="dot"></span> 
-                </div>
+               
 
                 <script>
                 var slideIndex = 0;
@@ -341,11 +305,9 @@ $maps_array= json_decode($maps_json,true);
                 }
                 slideIndex++;
                 if (slideIndex > slides.length) {slideIndex = 1}    
-                for (i = 0; i < dots.length; i++) {
-                dots[i].className = dots[i].className.replace(" active", "");
-                }
+                
                 slides[slideIndex-1].style.display = "block";  
-                dots[slideIndex-1].className += " active";
+                
                 setTimeout(showSlides, 2000); // Change image every 2 seconds
                
                 }
